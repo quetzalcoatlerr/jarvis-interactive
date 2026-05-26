5ca5fcc7ea12912fa1a3fed1e39d03e4142a9124
quetzal <examplerr950@gmail.com>
2026-05-26 15:42:28 +0200

    basic functionality, further additions through dev-func branch only

---
+++ modules/cmd.py
@@ -0,0 +1,30 @@
+import os
+import sys
+
+def handle_command(text: str) -> bool:
+    # Gets filtered text.
+    # Match -> execs the coomand through the ОS and returns True.
+    # If no match, returns False (reroute to LLM)
+    query = text.lower().strip()
+    
+    # CAT: Software launching
+    if "открой браузер" in query or "open browser" in query:
+        os.system("xdg-open https://google.com &")
+        return True
+        
+    if "терминал" in query or "terminal" in query:
+        # Lauch terminal emulator, I use default Kubuntu Konsole
+        os.system("konsole &")
+        return True
+
+    # CAT: Syscalls
+    if "выключи компьютер" in query or "shutdown system" in query:
+        os.system("poweroff")
+        return True
+
+    # CAT: Custom scripts (automation)
+    if "проверь контейнеры" in query or "docker status" in query:
+        os.system("konsole -e 'docker ps; read' &")
+        return True
+
+    return False
